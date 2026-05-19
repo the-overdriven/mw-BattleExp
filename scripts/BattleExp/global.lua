798b70f16d342a2417734a958e877adbb8c85fe7
the-overdriven <100090726+the-overdriven@users.noreply.github.com>
2026-05-20 00:39:40 +0200

    clean up global.lua

--- scripts/BattleExp/global.lua
+++ scripts/BattleExp/global.lua
@@ -4,25 +4,7 @@ local log = H.log
 local storage = require('openmw.storage')
 local summons = storage.globalSection('BattleExpSummons')
 
-local playerSummons = {}
-
 return {
-  interfaceName = 'BattleExp',
-  interface = {
-    registerSummon = function(actor)
-      log('registerSummon: %s', tostring(actor.id))
-      playerSummons[actor.id] = true
-    end,
-    unregisterSummon = function(actor)
-      log('unregisterSummon: %s', tostring(actor.id))
-      playerSummons[actor.id] = nil
-    end,
-    isPlayerSummon = function(actor)
-      local result = playerSummons[actor.id] == true
-      log('isPlayerSummon: %s -> %s', tostring(actor.id), tostring(result))
-      return result
-    end,
-  },
   eventHandlers = {
     RegisterPlayerSummon = function(actor)
       log('RegisterPlayerSummon event: %s', tostring(actor.id))
